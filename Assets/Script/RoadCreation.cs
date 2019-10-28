4d824f8b147a26947b89e7a87ef4fa295b2b28a9
Sashorg <1254-95@mail.ru>
2019-10-28 17:20:25 +0200

    RoadCreation
    Main Mechanics of the road were done+ 5 types of road prefabs

--- Assets/Script/RoadCreation.cs
+++ Assets/Script/RoadCreation.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadCreation : MonoBehaviour
 {
+    public static string direction;
 public    static int n = 0;
     static Vector3 vector3 = new Vector3(0f, 0f, 0f);
-    public Transform next_road_position;
+ public   static Quaternion rotator ;
+    private static  Transform next_road_position;
     public GameObject straight;
     public GameObject right45;
     public GameObject right90;
@@ -17,10 +20,14 @@ public    static int n = 0;
     // Start is     called before the first frame update
     private void Awake()
     {
+        next_road_position=GetComponent<Transform>();
         //        _tr.GetComponents<Transform>();
     }
     void Start()
     {
+        
+        next_road_position.position = vector3;
+       rotator= Quaternion.Euler(0f, 0f, 0f);
         //GameObject go = ObjectPoolingManager.Instance.GetObject(straight.name);
         //go.transform.position =  vector3;
         // go.transform.rotation = Quaternion.identity;
@@ -34,52 +41,56 @@ public    static int n = 0;
         {
             if (gameObject.transform.GetChild(0).gameObject.activeSelf == false)
             {
+                name_road(0);
                 print("we here"+ RandomRoadChooser.choose[0].ToString());
+              
                 GameObject go = ObjectPoolingManager.Instance.GetObject(RandomRoadChooser.choose[0].ToString());
+                print("we here" + next_road_position.position);
                 n++;
                 go.tag = n.ToString();
                 go.transform.position = next_road_position.position + vector3;
-                go.transform.rotation = next_road_position.rotation;
-                vector3.x = vector3.x + next_road_position.position.x;
-                vector3.y = vector3.y + next_road_position.position.y;
-                vector3.z = vector3.z + next_road_position.position.z;
+                go.transform.rotation = rotator;
+                next_road_position.position = go.gameObject.transform.GetChild(1).position;
                 DeleteRoad.CleanRoad();
+                RoadRotator.rotator();
                 print(n);
               
                 }
             }
         if (Input.GetKeyDown(KeyCode.N))
         {
+           
             if (gameObject.transform.GetChild(0).gameObject.activeSelf == false)
             {
+                name_road(1);
                 print("we here" + RandomRoadChooser.choose[1].ToString());
                 GameObject go = ObjectPoolingManager.Instance.GetObject(RandomRoadChooser.choose[1].ToString());
                 n++;
                 go.tag = n.ToString();
                 go.transform.position = next_road_position.position + vector3;
-                go.transform.rotation = next_road_position.rotation;
-                vector3.x = vector3.x + next_road_position.position.x;
-                vector3.y = vector3.y + next_road_position.position.y;
-                vector3.z = vector3.z + next_road_position.position.z;
+                go.transform.rotation = rotator;
+                next_road_position.position = go.gameObject.transform.GetChild(1).position;
                 DeleteRoad.CleanRoad();
+                RoadRotator.rotator();
                 print(n);
 
             }
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
+            
             if (gameObject.transform.GetChild(0).gameObject.activeSelf == false)
             {
-                print("we here" + RandomRoadChooser.choose[1].ToString());
+                name_road(2);
+                print("we here" + RandomRoadChooser.choose[2].ToString());
                 GameObject go = ObjectPoolingManager.Instance.GetObject(RandomRoadChooser.choose[2].ToString());
                 n++;
                 go.tag = n.ToString();
                 go.transform.position = next_road_position.position + vector3;
-                go.transform.rotation = next_road_position.rotation;
-                vector3.x = vector3.x + next_road_position.position.x;
-                vector3.y = vector3.y + next_road_position.position.y;
-                vector3.z = vector3.z + next_road_position.position.z;
+                go.transform.rotation = rotator;
+                next_road_position.position = go.gameObject.transform.GetChild(1).position;
                 DeleteRoad.CleanRoad();
+                RoadRotator.rotator();
                 print(n);
 
             }
@@ -87,4 +98,8 @@ public    static int n = 0;
 
     }
 
+    private void name_road(int i)
+    {
+       direction= RandomRoadChooser.choose[i].ToString();
     }
+}
---
+++ Assets/Script/RoadRotator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void rotator() {
+        switch (RoadCreation.direction) {
+            case "straight":
+                RoadCreation.rotator = RoadCreation.rotator * Quaternion.Euler(0, 0f, 0f);
+                break;
+            case "right45":
+                RoadCreation.rotator = RoadCreation.rotator * Quaternion.Euler(0, 45f, 0f);
+                print("Your rotating is"+RoadCreation.rotator);
+
+                break;
+            case "right90":
+                RoadCreation.rotator = RoadCreation.rotator* Quaternion.Euler(0, 90f, 0f);
+                print("Your rotating is" + RoadCreation.rotator);
+
+                break;
+            case "left45":
+                RoadCreation.rotator = RoadCreation.rotator * Quaternion.Euler(0, -45f, 0f);
+                print("Your rotating is" + RoadCreation.rotator);
+
+                break;
+            case "left90":
+                RoadCreation.rotator = RoadCreation.rotator * Quaternion.Euler(0, -90f, 0f);
+                print("Your rotating is" + RoadCreation.rotator);
+
+                break;
+
+        }
+    }
+}
