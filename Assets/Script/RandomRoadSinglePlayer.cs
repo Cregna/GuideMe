56e103c40bdbbe0ed1087b476b57c5de6d3a7297
Regna <cristian.regna@mail.polimi.it>
2019-11-10 14:35:04 +0100

    SelfCar driving

--- Assets/Script/RandomRoadSinglePlayer.cs
+++ Assets/Script/RandomRoadSinglePlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Utility;
 
 public class RandomRoadSinglePlayer : MonoBehaviour
 {
@@ -9,10 +10,13 @@ public class RandomRoadSinglePlayer : MonoBehaviour
     static Vector3 vector3 = new Vector3(0f, 0f, 0f);
     public static Quaternion rotator;
     private static Transform next_road_position;
-
+    private DinamicWaypoints dinamicWaypoints;
+    private WaypointRoute myroute;
+    
     static bool flag = true;
     float elapsed = 0f;
     public float ntime = 2f;
+    [SerializeField] private GameObject waypoints;
     [SerializeField] private GameObject[] myArray;
     
     
@@ -27,7 +31,6 @@ public class RandomRoadSinglePlayer : MonoBehaviour
 
     void Start()
     {
-        
         next_road_position.position = vector3;
         rotator = Quaternion.Euler(0f, 0f, 0f);
         //GameObject go = ObjectPoolingManager.Instance.GetObject(straight.name);
@@ -60,13 +63,30 @@ public class RandomRoadSinglePlayer : MonoBehaviour
             go.transform.position = next_road_position.position + vector3;
             go.transform.rotation = rotator;
             next_road_position.position = go.gameObject.transform.GetChild(1).position;
-
+            
             //DeleteRoad.CleanRoadSingle();
             RoadRotator.rotatorsingle();
-            
+            Deletewaypoints();
+            setwaypoints(go.gameObject.transform.GetChild(0), go.gameObject.transform.GetChild(1));
+            myroute.UpdateNodes();
+
         }
     }
-     
+
+     private void Deletewaypoints()
+     {
+         if (waypoints.transform.childCount > 2)
+         {
+             GameObject.Destroy(waypoints.transform.GetChild(waypoints.transform.childCount - 1).gameObject);
+         }
+
+     }
+
+     private void setwaypoints(Transform waypoint1, Transform waypoint2)
+     {
+         waypoint1.SetParent(waypoints.transform);
+         waypoint2.SetParent(waypoints.transform);
+     }
      private void name_road(int i)
      {
          direction= myArray[i].name.ToString();
