3b0f772725dd0825b64677c95e3b75e9177b54c4
Regna Cristian <44031292+Regnac@users.noreply.github.com>
2019-11-24 23:11:34 +0100

    single brach

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,12 +8,14 @@ namespace UnityStandardAssets.Vehicles.Car
     [RequireComponent(typeof (CarController))]
     public class CarUserControl : MonoBehaviour
     {
+        public bool photon;
         private PhotonView myPhotonView;
         private CarController m_Car; // the car controller we want to use
 
 
         private void Awake()
         {
+            photon = false;
             // get the car controller
             m_Car = GetComponent<CarController>();
         }
@@ -26,8 +28,24 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
-            if (myPhotonView.IsMine)
+            if (photon)
             {
+                if (myPhotonView.IsMine)
+                {
+                    // pass the input to the car!
+                    float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                    float v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+                    float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+                    m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+                }
+            }
+            else
+            {
+
                 // pass the input to the car!
                 float h = CrossPlatformInputManager.GetAxis("Horizontal");
                 float v = CrossPlatformInputManager.GetAxis("Vertical");
@@ -39,5 +57,7 @@ namespace UnityStandardAssets.Vehicles.Car
 #endif
             }
         }
+    
+  
     }
 }
