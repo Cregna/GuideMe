bff5f295b8dcb31a6ae37c125eb30d24ca5f1214
Regna Cristian <44031292+Regnac@users.noreply.github.com>
2019-12-29 12:01:52 +0100

    added arcade car in single mode, just tuning

---
+++ Assets/Script/PowerUpBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBox : MonoBehaviour
+{
+
+    public float pushforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("CAR ENTER");
+        GameObject go = GameObject.FindGameObjectWithTag("Player"); 
+        go.GetComponent<Rigidbody>().AddForce(Vector3.back * pushforce, ForceMode.Acceleration);
+    }
+}
