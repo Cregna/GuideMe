f6ddb3cdb050fbdab3d1ba57d89386c6a5572d40
Regna Cristian <44031292+Regnac@users.noreply.github.com>
2020-01-02 14:49:01 +0100

    Online UI adapt based on screen resolution

--- Assets/Script/TopFollowCamera.cs
+++ Assets/Script/TopFollowCamera.cs
@@ -9,13 +9,29 @@ public class TopFollowCamera : MonoBehaviour
 
     public float smoothSpeed = 0.125f;
     public float rotation;
+    public int uibox; 
     public Vector3 offset;
-    
 
-    
+
+    private void Start()
+    {
+        switch (uibox)
+        {
+            case 1:
+                target = GameObject.FindGameObjectsWithTag("spawn1")[0].transform;
+                break;
+            case 2:
+                target = GameObject.FindGameObjectsWithTag("spawn2")[0].transform;
+                break;
+            case 3:
+                target = GameObject.FindGameObjectsWithTag("spawn3")[0].transform;
+                break;
+        }
+    }
+
     void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(new Vector3(rotation, 180, 0));
+        transform.rotation = Quaternion.Euler(new Vector3(rotation, 0, 0));
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
